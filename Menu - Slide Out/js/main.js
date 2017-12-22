9770b2a2979a08495c48400f337223bffa0ace74
William Roger <williamroger.frontend@gmail.com>
2017-12-21 22:33:45 -0300

    Menu Slide Out (HTML5, CSS3 e jQuery)

---
+++ Menu - Slide Out/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$("#menuIcon").click(function() {
+		$("#mainMenu").css("left", "0px");
+		function showMenu() {
+			$("#mainMenu").css("-webkit-clip-path", "polygon(0 0, 100% 0, 100% 100%, 0% 100%)");
+			$("#menuIcon").animate({right:'-100'}, 300);
+		}
+		setTimeout(showMenu, 100);
+	});
+
+	$("#close").click(function() {
+		$("#mainMenu").css("-webkit-clip-path", "polygon(0 0, 0% 0, 100% 100%, 0% 100%)");
+		function hideMenu() {
+			$("#mainMenu").css("left", "-300px");
+			$("#menuIcon").animate({right: '50'}, 300);
+		}
+
+		setTimeout(hideMenu, 300);
+
+		function originalLayout() {
+			$("#mainMenu").css("-webkit-clip-path", "polygon(0 0, 100% 0, 0% 100%, 0% 100%)");
+		}
+
+		setTimeout(originalMenu);
+		
+	});
+
+});
